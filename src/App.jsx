8bfa4a57f4f16b9a11685c95961266ad58e82fc5
stoddow313 <wbs@uoregon.edu>
2026-03-11 13:12:20 -0600

    Remove default Vite Template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Diamond Metrics</h1>
+      <p>Baseball analytics for high school programs.</p>
+    </div>
+  )
+}
+
+export default App
