98c38445b039feb3f8c0392a0ae6f719883b0c59
stoddow313 <wbs@uoregon.edu>
2026-03-11 22:11:36 -0600

    Update to brandmark

--- src/components/BrandMark.jsx
+++ src/components/BrandMark.jsx
@@ -3,7 +3,9 @@ import logo from "../assets/diamond-logo.svg";
 function Brandmark() {
   return (
     <div className="brandmark">
-      <img src={logo} alt="Diamond Metrics logo" className="brandmark-icon" />
+      <div className="brandmark-badge">
+        <img src={logo} alt="Diamond Metrics logo" />
+      </div>
 
       <div className="brandmark-text">
         <span className="brandmark-title">Diamond Metrics</span>
